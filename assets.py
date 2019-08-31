d0600b6d40d531239651412268eb3ca5a27f5c0d
Gustavo Marques <gustavomarques.gml@gmail.com>
2019-08-31 14:11:45 -0300

    Start of changing menu

--- assets.py
+++ assets.py
@@ -5,6 +5,8 @@ from pygame.locals import *
 class Palette:
     # Paleta de cores https://javier.xyz/cohesive-colors/
     # Primeira paleta
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
     COLOR_1 = (85, 94, 123)
     COLOR_2 = (183, 217, 104)
     COLOR_3 = (181, 118, 173)
@@ -14,47 +16,54 @@ class Palette:
     COLOR_7 = (104, 188, 39)
     COLOR_8 = (35, 150, 170)
 
-class Assets:
-    LINE_THICKNESS = 7
-    NODE_RADIUS = 40
-    def get_positions(self, tam):
-        # Posições dos nós nos grafos de determinados tamanhos
-        if tam==3:
-            return [ (150, 150), (300, 450), (450, 150) ]
-        elif tam==4:
-            return [ (150, 150), (150, 450), (450, 150), (450, 450) ]
-        else:
-            return [(100, 100)]*tam
+LINE_THICKNESS = 7
+NODE_RADIUS = 40
 
-    def draw_graph(self, game, graph):
-        game.circle_radius = graph.radius
-        positions = self.get_positions(self, graph.tam)
-        for i in range(graph.tam):
-            if graph.color[i]==graph.UNCOLORED:
-                self.draw_node(self, game, positions[i])
-            elif graph.color[i]==graph.BLUE:
-                self.draw_node(self, game, positions[i], color=Palette.COLOR_6)
-            elif graph.color[i]==graph.GREEN:
-                self.draw_node(self, game, positions[i], color=Palette.COLOR_7)
-            elif graph.color[i]==graph.RED:
-                self.draw_node(self, game, positions[i], color=Palette.COLOR_4)
-            else:
-                print(graph.color[i])
-                
-        for u, v in graph.edges_list:
-             self.draw_edge(self, game, positions[u], positions[v])
+def get_positions(tam):
+    # Posições dos nós nos grafos de determinados tamanhos
+    if tam==3:
+        return [ (150, 150), (300, 450), (450, 150) ]
+    elif tam==4:
+        return [ (150, 150), (150, 450), (450, 150), (450, 450) ]
+    else:
+        return [(100, 100)]*tam
+
+def draw_text(game, text, position, font_size=12, color=Palette.WHITE ):
+    txt = pygame.font.Font('freesansbold.ttf',font_size)
+    TextSurf = txt.render(text, True, color)
+    TextRect = TextSurf.get_rect()
+    TextRect.center = (position)
+    game.screen.blit(TextSurf, TextRect)
+
+def draw_graph(game, graph):
+    game.circle_radius = graph.radius
+    positions = get_positions(graph.tam)
+    for i in range(graph.tam):
+        if graph.color[i]==graph.UNCOLORED:
+            draw_node(game, positions[i])
+        elif graph.color[i]==graph.BLUE:
+            draw_node(game, positions[i], color=Palette.COLOR_6)
+        elif graph.color[i]==graph.GREEN:
+            draw_node(game, positions[i], color=Palette.COLOR_7)
+        elif graph.color[i]==graph.RED:
+            draw_node(game, positions[i], color=Palette.COLOR_4)
+        else:
+            print(graph.color[i])
             
-    def draw_node(self, game, position, color=Palette.COLOR_8):
-        pygame.draw.circle(game.screen, color, position, game.circle_radius)
+    for u, v in graph.edges_list:
+            draw_edge(game, positions[u], positions[v])
         
-    def draw_edge(self, game, pos1, pos2, color=Palette.COLOR_1):
-        a = pos1[1]-pos2[1]
-        b = pos2[0]-pos1[0]
-        r = game.circle_radius
-        theta = math.atan2(a, b)
-        x1 = pos1[0]+math.cos(theta)*r
-        y1 = pos1[1]-math.sin(theta)*r
-        x2 = pos2[0]-math.cos(theta)*r
-        y2 = pos2[1]+math.sin(theta)*r
-        pygame.draw.line(game.screen, color, (x1, y1), (x2, y2), self.LINE_THICKNESS)
+def draw_node(game, position, color=Palette.COLOR_8):
+    pygame.draw.circle(game.screen, color, position, game.circle_radius)
+    
+def draw_edge(game, pos1, pos2, color=Palette.COLOR_1):
+    a = pos1[1]-pos2[1]
+    b = pos2[0]-pos1[0]
+    r = game.circle_radius
+    theta = math.atan2(a, b)
+    x1 = pos1[0]+math.cos(theta)*r
+    y1 = pos1[1]-math.sin(theta)*r
+    x2 = pos2[0]-math.cos(theta)*r
+    y2 = pos2[1]+math.sin(theta)*r
+    pygame.draw.line(game.screen, color, (x1, y1), (x2, y2), LINE_THICKNESS)
 
--- game.py
+++ game.py
@@ -2,10 +2,14 @@ import pygame
 import math
 from pygame.locals import *
 from assets import Palette
-from assets import Assets
+from assets import draw_text
+from assets import draw_graph
+
+
+
 
 class Game:
-    WIDTH = 600
+    WIDTH = 800
     HEIGHT = 600
     GAME_NAME = 'Bipartite graph game'
     running = True
@@ -14,11 +18,22 @@ class Game:
     def build_screen(self):
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.screen.fill(Palette.COLOR_5)
+        self.clock = pygame.time.Clock()
         pygame.display.set_caption(self.GAME_NAME)
-        a = self.graphs[0].bipartite()
-        print(a)
-        Assets.draw_graph(Assets, game=self, graph=self.graphs[0])
     
+    def print_graph(self, graph):
+        self.clock.tick(15)
+        graph.bipartite()
+        draw_graph(game=self, graph=graph)
+
+    def print_menu(self):
+        self.clock.tick(15)
+        self.screen.fill(Palette.COLOR_1)
+        draw_text(game=self, text='Bipartite graph game', position=((self.WIDTH/2),(100)), font_size=60)
+
+        pygame.draw.rect(self.screen, Palette.COLOR_6,(150,450,100,50))
+        pygame.draw.rect(self.screen, Palette.COLOR_8,(550,450,100,50))
+        
     def run(self, graphs):
         self.graphs = graphs
         pygame.init()
@@ -27,5 +42,6 @@ class Game:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
+            self.print_menu()
             pygame.display.update()
 
